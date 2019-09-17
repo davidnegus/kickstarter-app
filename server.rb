447460fcfb45f548e55adb6052bcae84a7e7c777
David <david_2_9@hotmail.co.uk>
2019-09-17 13:02:29 +0100

    changes

--- server.rb
+++ server.rb
@@ -4,6 +4,30 @@ require_relative "./lib/kickstarter.rb"
 
 class Server < Sinatra::Base
 
+    enable :sessions
+
+    before do
+        puts params
+        puts session[:user]
+        if session[:user].nil? && request.path != "/users/new"
+            
+            unless params["name"]
+                redirect "/users/new"
+            end
+        end
+
+    end
+
+    get "/users/new" do
+        erb :user
+    end
+
+    post "/users" do
+        user = User.create(params)
+        session[:user] = user #key value pair, a ruby hash
+        erb :index, locals: {projects: Project.all}, layout: :layout
+    end
+
     get /\/|\/projects/ do
         erb :index, locals: {projects: Project.all}, layout: :layout
     end
@@ -29,6 +53,23 @@ class Server < Sinatra::Base
     post "/projects/:id" do |id|
         project = Project.find(id)
         project.update(params)
+        erb :index, locals: {projects: Project.all}, layout: :layout
+    end
+
+    get "/projects/:id" do |id|
+        project = Project.find(id)
+        
+        erb :project, locals: {project: Project.find(id)}, layout: :layout
+    end
+
+    post "/projects/:id/donate" do |id|
+        project = Project.find(id)
+        project.pledges.create(user_id: session[:user].id, amount: params["amount"])
+        erb :project, locals: {project: project}, layout: :layout
+    end
+
+    get "/users/logout" do
+        session[:user] = nil
         erb :index, locals: {projects: Project.all}, layout: :layout
     end
 end
